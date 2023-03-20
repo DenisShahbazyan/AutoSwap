cc5c73a4f4e1d9b8b5dfc5dc9cba0983541ca3b2
Denis Shahbazyan <den.shahbazyan@yandex.ru>
2023-03-21 01:23:11 +0300

    Обновил README.MD

--- src/Offsets.h
+++ src/Offsets.h
@@ -22,6 +22,24 @@ struct Offset
 	static const DWORD GroupPristWID = 0xC;
 };
 
+//struct Offset
+//{
+//	static const DWORD BaseAddress = 0x1411B7530; //ок
+//	static const DWORD GameAddress = 0x1411b8178; //ок
+//	static const DWORD PackCall = 0x00819A40;
+//
+//	static const DWORD GameStructure = 0x38; //ок
+//	static const DWORD PlayerStructure = 0x60; //ок
+//
+//	static const DWORD PlayerHP = 0x62C; //ок
+//	static const DWORD PlayerTarget = 0x5A4;
+//	static const DWORD ClanID = 0x694;
+//	static const DWORD PlayerName = 0x6FC;
+//
+//	static const DWORD GroupLeaderWID = 0xC;
+//	static const DWORD GroupPristWID = 0xC;
+//};
+
 extern Offset of;
 
 #endif // GLOBAL_STRUCT_H
